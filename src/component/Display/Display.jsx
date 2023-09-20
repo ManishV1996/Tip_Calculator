4fb284d607fd6e0eba3671cf7b2e2308b653880f
Manish Verma <vermamanish098@gmial.com>
2023-09-21 03:40:53 +0530

    first commit for tip calculator app

--- src/component/Display/Display.jsx
+++ src/component/Display/Display.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { useState } from 'react';
-import Header from "../header/header";
-import Input from "../input/input";
 import './Display.css';
-import Output from "../output/output";
-import Footer from "../footer/footer";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Output from "../Output/Output";
+import Input from "../Input/Input";
 
 
 const Display=()=>{
